2485e5fde300d50fa30cd45395a16422071149f1
Branson Morgan <bransox@comcast.net>
2022-08-10 19:17:18 -0400

    commit tree 8847549d5923d211f6c06bfb87d6db0bfcd30460

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function(fare) {
+        return (x * fare)
+    }
+
+}
+const fareDoubler = function createFareMultiplier(fare) {
+    return (fare * 2);
+}
+const fareTripler = function createFareMultiplier(fare) {
+    return (fare * 3);
+}
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+
+}
